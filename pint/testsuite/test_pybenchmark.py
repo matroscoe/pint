ed8060b56f9af335ac3fb9940594816c54169920
Mat Rosoe <102177474+matroscoe@users.noreply.github.com>
2023-06-06 00:10:16 +0000

    better test organization

--- pint/testsuite/test_pybenchmark.py
+++ pint/testsuite/test_pybenchmark.py
@@ -5,18 +5,30 @@ unit_registry = pint.get_application_registry()
 
 
 @pytest.mark.parametrize(
-    "input",
+    "input_string",
     [
         "42.123 m",
         "42.123 m^2",
         "42.123 m/s^2",
+    ],
+)
+def test_quantity_create_single_input(func_registry, benchmark, input_string):
+    """
+    Benchmark how long quantity creation takes with a range of Quantity unit strings
+    """
+    benchmark(func_registry.Quantity, input_string)
+
+
+@pytest.mark.parametrize(
+    "value, unit_string",
+    [
         [42.123, "m"],
         [42.123, "m^2"],
         [42.123, "m/s^2"],
     ],
 )
-def test_quantity_create(benchmark, input):
+def test_quantity_create_split_input(func_registry, benchmark, value, unit_string):
     """
     Benchmark how long quantity creation takes with a range of Quantity unit strings
     """
-    benchmark(unit_registry.Quantity, input)
+    benchmark(func_registry.Quantity, value, unit_string)
